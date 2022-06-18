63b07f3d0ab4eaf63eb8236960c0116b2c09b8bd
‘DashISme’ <1298625150@qq.com>
2022-06-18 13:47:37 +0800

    first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,47 @@
+module.exports = {
+  title: 'Hello VuePress',
+  description: 'Just playing around',
+  head: [
+    ['link', { rel: 'icon', content: '/favicon.ico' }],
+    ['mate', { name: 'author', content: 'Dash' }],
+    ['mate', { name: 'keywords', content: 'vuepress,Dash' }]
+  ],
+    themeConfig: {
+      lastUpdated: '更新时间', 
+      sidebar: 'auto',
+      logo: '/assets/img/logo.png',
+      nav: [
+        { text: 'Home', link: '/' },
+        { text: 'Guide', link: '/about' },
+        {
+          text: 'Languages',
+          items: [
+            { text: 'Group1', items: [
+            { text: 'Home', link: '/' },
+            { text: 'Guide', link: '/about' }
+          ] },
+            { text: 'Group2', items: [
+              { text: 'Home', link: '/' },
+              { text: 'Guide', link: '/about' }
+            ] }
+          ]
+        },
+        { text: 'External', link: 'https://google.com' },
+      ],
+      sidebar: {
+        "/css/": [
+          'c-aaa',
+            'c-bbb',
+            'c-ccc'
+        ],
+        "/javascirpt/": [
+          'j-aaa',
+            'j-bbb',
+            'j-ccc'
+        ],
+        "/":[
+          ""
+        ]
+      }
+    }
+  }
